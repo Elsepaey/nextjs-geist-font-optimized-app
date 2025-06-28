7feb4d2e9d0118bcd3f37718aef1ce09e7a5f2f2
ahmed ehab elasepaey <105998967+Elsepaey@users.noreply.github.com>
2025-06-28 16:41:55 +0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1751118117569

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_9fb6240e._.js",
+  "static/chunks/src_b2169d4e._.js"
+],
+    source: "dynamic"
+});
